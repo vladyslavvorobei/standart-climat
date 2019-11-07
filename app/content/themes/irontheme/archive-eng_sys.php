b35afef3263492ca138c98ff36ac6ab21247d9c1
vladyslavvorobei <47365718+vladyslavvorobei@users.noreply.github.com>
2019-11-07 17:16:02 +0200

    Update archive-eng_sys.php

--- app/content/themes/irontheme/archive-eng_sys.php
+++ app/content/themes/irontheme/archive-eng_sys.php
@@ -3,7 +3,7 @@
   <section class="engin-sys">
     <div class="container">
       <div class="title title--no-number">
-        <h2> информация <strong> Инженерные системы </strong></h2>
+        <h2 data-aos="fade-right" data-aos-offset="600"> информация <strong data-aos="fade-up" data-aos-offset="600"> Инженерные системы </strong></h2>
       </div>
       <div class="engin-sys__description"> Компания "Стандарт Климат" выполняет полный цикл работ начиная с <strong>проекта</strong> и заканчивая сервисным обслуживанием. </div>
 
@@ -12,7 +12,7 @@
       if ($sys->have_posts()): ?>
         <div class="engin-sys__grid">
           <?php while ($sys->have_posts()): $sys->the_post(); ?>
-            <a href="<?php the_permalink(); ?>">
+            <a href="<?php the_permalink(); ?>" data-aos="fade-up" data-aos-duration="650">
               <?php the_post_thumbnail('medium'); ?>
               <h3><?php the_title(); ?></h3>
             </a>
