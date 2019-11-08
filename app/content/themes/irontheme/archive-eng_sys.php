eaf1076d49dfad698f07c86c05c7e848bb10b094
transoman <vasiag10@gmail.com>
2019-11-08 08:35:03 +0200

    Added a delay for all items

--- app/content/themes/irontheme/archive-eng_sys.php
+++ app/content/themes/irontheme/archive-eng_sys.php
@@ -11,8 +11,8 @@
       $sys = get_any_post( 'eng_sys', -1 );
       if ($sys->have_posts()): ?>
         <div class="engin-sys__grid">
-          <?php while ($sys->have_posts()): $sys->the_post(); ?>
-            <a href="<?php the_permalink(); ?>" data-aos="fade-up" data-aos-duration="650">
+          <?php $i = 0; while ($sys->have_posts()): $sys->the_post(); ?>
+            <a href="<?php the_permalink(); ?>" data-aos="fade-up" data-aos-duration="650" data-aos-delay="<?php echo $i++ * 100; ?>">
               <?php the_post_thumbnail('medium'); ?>
               <h3><?php the_title(); ?></h3>
             </a>
